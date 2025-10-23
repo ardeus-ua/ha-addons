b8b9dc58b42f1a1a64a8cb948a228c4551cd75cf
ardeus-ua <ardeus@gmail.com>
2025-10-23 15:08:48 +0300

    Update run.sh

--- battery_soc_page/run.sh
+++ battery_soc_page/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start Cloudflare Tunnel in background
-cloudflared tunnel run --token "${tunnel_token}" &
+cloudflared tunnel run --url http://localhost:5000 ${tunnel_id} &
 
 # Start Flask server
 python app.py
